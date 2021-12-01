50191f88102f147c23cedebeba4184d92c785efe
July911 <min95911@nate.com>
2021-12-01 23:59:48 +0900

    feat: BaseballGame class 생성 및 내부 구현 #1

--- ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/BaseballGame.swift
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/BaseballGame.swift
@@ -1,3 +1,14 @@
 import Foundation
 
-
+class BaseballGame {
+    let catcher: [Int]
+    
+    func playGame() {
+        
+    }
+    
+    init(catcher: [Int]) {
+        self.catcher = catcher
+    }
+    
+}
