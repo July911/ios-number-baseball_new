3e4f5bfccc88d2a0101e51b19477495a24f79646
July911 <min95911@nate.com>
2021-12-02 00:51:20 +0900

    feat: 스트라이크와 볼을 구분하는 함수 구현 #1

--- ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/GameManager.swift
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/GameManager.swift
@@ -3,8 +3,10 @@ import Foundation
 class GameManager {
     var life = 9
     
-    func checkStrikeOrBall(_ : [Int], _ : [Int]) -> (strike: Int, ball: Int) {
-        
-        return (0,0)
+    func checkStrikeOrBall(_ hit: [Int], _ target: [Int]) -> (strike: Int, ball: Int) {
+        let ballcountOfEachTurn = 0...2
+        let sameNumberCount = hit.filter { target.contains($0) }.count
+        let strikeCount = ballcountOfEachTurn.filter { hit[$0] == target[$0] }.count
+        return (strikeCount,sameNumberCount - strikeCount)
     }
 }
---
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/NumberBaseballTest.swift
@@ -0,0 +1,15 @@
+//import XCTest
+//
+//class NumberBaseballTest: XCTestCase {
+//
+//    override func setUpWithError() throws {
+//        self.setUpWithError()
+//    }
+//
+//    override func tearDownWithError() throws {
+//        self.tearDownWithError()
+//    }
+//
+//    func test_
+//
+//}
