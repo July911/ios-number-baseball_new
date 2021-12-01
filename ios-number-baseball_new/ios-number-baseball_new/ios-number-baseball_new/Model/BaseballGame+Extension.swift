d86bbd4ccb8504173f6dc80df45e100a3db4f80b
July911 <min95911@nate.com>
2021-12-02 00:05:59 +0900

    feat: 임의의 수 3개를 반환하는 함수 extension 내부에 구현 #1

--- ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/BaseballGame+Extension.swift
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/BaseballGame+Extension.swift
@@ -1,1 +1,15 @@
 import Foundation
+
+extension BaseballGame {
+    func creatComputerNumbers() -> [Int] {
+        var numberRange = 1...9
+        var availableCount = 3
+        var computerNumbers = [Int]()
+        
+        repeat {
+            computerNumbers.append(Int.random(in: numberRange))
+        } while Set(computerNumbers).count == availableCount
+        
+        return computerNumbers
+}
+}
