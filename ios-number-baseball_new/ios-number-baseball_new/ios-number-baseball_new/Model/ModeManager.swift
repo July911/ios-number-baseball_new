507bf0f0a90697eb05f9f7050dbdec3212125680
July911 <min95911@nate.com>
2021-12-01 23:43:00 +0900

    feat: ModeManager class 생성 및 내부 구현 #1

--- ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/ModeManager.swift
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/ModeManager.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+class ModeManager {
+    var gameMode: GameMode
+    
+    func changeGameMode(_ currentMode: inout GameMode) {
+        gameMode = currentMode
+    }
+    init(gameMode:GameMode) {
+        self.gameMode = gameMode
+    }
+}
