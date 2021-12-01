7a0b2b0705aa9beef15768f35eec9fa1e9b38f69
July911 <min95911@nate.com>
2021-12-01 23:54:56 +0900

    feat: GameManager class 및 스트라이크와 볼을 체크하는 함수 생성 #1

--- ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/GameManager.swift
+++ ios-number-baseball_new/ios-number-baseball_new/ios-number-baseball_new/Model/GameManager.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+class GameManager {
+    var life = 9
+    
+    func checkStrikeOrBall(_ : [Int], _ : [Int]) -> (strike: Int, ball: Int) {
+        
+        return (0,0)
+    }
+}
